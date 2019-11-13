07cb42bdcf79e33403d503b441937e5644cc91c3
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-13 14:00:52 +0100

    cors install

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
+
+
 const parserMiddleware = bodyParser.json()
 const app = express()
-const port = process.env.PORT || 4000
+const corsMiddleware = cors()
 
+const port = process.env.PORT || 4000
+app.use(corsMiddleware)
 app.use(parserMiddleware)
 
 app.listen(port, () => {
