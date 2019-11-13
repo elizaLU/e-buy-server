dbb8089203203e80c78086176d3c0c93512bc21a
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-13 13:47:00 +0100

    cleanup

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const parserMiddleware = bodyParser.json()
-
 const app = express()
 const port = process.env.PORT || 4000
+
 app.use(parserMiddleware)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-//const db = require('./db') already imported in model
 const adRouter = require('./ads/router')
 app.use(adRouter)
